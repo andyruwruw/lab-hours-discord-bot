4c45056ddceaf22ee46231cea4a92d6f759ef9cd
Andrew Young <andrew@youngshome.com>
2020-03-17 16:40:03 -0600

    Un-cancel

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ta-lab-discord-bot
+
+Discord Bot for managing teacher assistant lab schedule. COVID-19 forced us to push all of our lab hours for questions to an online service.
+
--- src/scripts/bot.js
+++ src/scripts/bot.js
@@ -1,41 +1,56 @@
 // Dependencies
 let Discord = require('discord.js');
-
 const mongoose = require("mongoose");
-
-mongoose.connect('mongodb://localhost:27017/tabot', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 var add = require('date-fns/add');
 var formatDistanceToNow = require('date-fns/formatDistanceToNow')
 var isPast = require('date-fns/isPast')
 var isFuture = require('date-fns/isFuture')
 
+// Connecting to database.
+mongoose.connect('mongodb://localhost:27017/tabot', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Models
 let TA = require('./ta.js');
 let LabHour = require('./labhour.js');
 
-
-
-// Inherits from Discord.js Client
+/**
+ * TaBot Class
+ * Inherits from Discord.js Client
+ */
 class TaBot extends Discord.Client {
+    /**
+     * TaBot Constructor
+     * Returns new instance of client.
+     * 
+     * @param {object} options Discord Secret token.
+     */
     constructor(options) {
-        // Running Parent Class Constructor
-        super();
-        // Defining Member Function for Server Connection
+        super(options);
         this.on('ready', this.handleConnect);
-        // Defining Member Function for Message Event
         this.on('message', this.handleMessage);
     }
 
+    /**
+     * Handle Connect
+     * Runs upon bot's connection to server.
+     */
     handleConnect() {
         console.log('Logged in as BOT');
     }
 
+    /**
+     * Handle Message
+     * Runs when bot sees a message. 
+     * 
+     * @param {class} message Discord.js message object.
+     */
     async handleMessage(message) {
-        console.log("Handle");
+        // Checks if it's a command.
         if (message.content.substring(0, 1) != '/') return;
+        // Parses arguments
         let args = message.content.substring(1).split(' ');
         let cmd = args[0];
         args = args.splice(1);
@@ -46,8 +61,13 @@ class TaBot extends Discord.Client {
             case 'cancel':
                 this.cancel(message, args);
                 break;
+            case 'uncancel':
+                this.uncancel(message, args);
+                break;
+            case 'gethours':
+                this.getHours(message, args);
+                break;
             case 'taschedule':
-                console.log("calling");
                 this.taSchedule(message);
                 break;
             default:
@@ -56,6 +76,12 @@ class TaBot extends Discord.Client {
         }
     }
 
+    /**
+     * Invalid Message
+     * Returns message stating error.
+     * 
+     * @param {class} message Discord.js message object.
+     */
     invalidCommand(message) {
         const embed = new Discord.MessageEmbed()
         .setTitle('Oops! Invalid Command')
@@ -65,78 +91,229 @@ class TaBot extends Discord.Client {
         return false;
     }
 
+    /**
+     * Help
+     * Returns embed with available commands.
+     * 
+     * @param {class} message Discord.js message object.
+     */
     help(message) {
         const embed = new Discord.MessageEmbed()
         .setTitle('Available Commands')
         .setColor(0x2bff99)
-        .setDescription('`/help`: Displays available Commands.\n`/taschedule`: Shows current or next TA scheduled and any exceptions.');
+        .setDescription('`/help`: Displays available Commands.\n`/taschedule`: Shows current or next TA scheduled and any exceptions.\n`/gethours name`: Shows a given TA\'s hours');
         message.reply(embed);
         return false;
     }
 
-    async cancel(message, params) {
-        let returnMessage = "";
-        if (params.length == 3) {
-            let name = params[0].charAt(0).toUpperCase() + params[0].substring(1);
-            let ta = await TA.findOne({
-                name: name,
-            });
-            let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-            const isSame = (element) => element == params[1];
-            let dayIndex = days.findIndex(isSame);
-            let event = null;
-            if (ta != null) {
-                if (dayIndex != -1) {
+    /**
+     * Get Hours
+     * Returns message with a given TA's Hours
+     * 
+     * @param {class} message Discord.js message object.
+     * @param {array} params Array of strings of other arguments from message.
+     */
+    async getHours(message, params) {
+        try {
+            let name = null;
+            let returnMessage = "";
+            if (params.length > 0) {
+                name = params[0].charAt(0).toUpperCase() + params[0].substring(1);
+                let ta = await TA.findOne({
+                    name: name,
+                });
+                if (ta != null) {
+                    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
                     let hours = await LabHour.find({
                         ta: ta._id,
                     });
                     for (let i = 0; i < hours.length; i++) {
                         let start = new Date(hours[i].start);
+                        let end = new Date(hours[i].end);
                         let day = start.getDay();
                         let hour = start.getHours();
-                        if (day == dayIndex && hour == parseInt(params[2], 10)) {
-                            event = hours[i];
-                            await LabHour.updateOne({
-                                _id: hours[i]._id,
-                            }, {
-                                $set: {
-                                    canceled: true,
-                                }
-                            });
-                            break;
-                        }
-                    }
-                    if (event) {
-                        let day = days[(new Date(event.start)).getDay()].charAt(0).toUpperCase() + days[(new Date(event.start)).getDay()].substring(1);
-                        let hour = new Date(event.start).getHours();
-                        returnMessage = "Success: Lab Hours at " + hour + " on " + day + " for **" + name + "** has been canceled";
-                    } else {
-                        returnMessage = "**Error**: No Lab Hour scheduled at that time.\n";
+                        let endHour = end.getHours();
+                        returnMessage += days[day] + ": " + this.editHour(hour) + " to " + this.editHour(endHour) + "\n";
                     }
                 } else {
-                    returnMessage = "**Error**: Incorrect Day.\nTry: monday, tuesday, wednesday, thursday, friday, saturday";
+                    returnMessage = "**Error**: TA does not exist.\nContact Andrew. He screwed up somewhere.";
                 }
             } else {
-                returnMessage = "**Error**: TA does not exist.\nContact Andrew. He screwed up somewhere.";
+                returnMessage = "**Error**: Provide a TA's Name.\n**Command**: `/gethours name`\n**Example**: `/gethours andrew`";
             }
-        } else {
-            returnMessage = "**Error**: Forgot a parameter.\n**Cancel Command:**`/cancel name day hour`\n**Example:** /cancel bob tuesday 14";
+            const embed = new Discord.MessageEmbed()
+            .setTitle(name + '\'s Hours:')
+            .setColor(0x25faf6)
+            .setDescription(returnMessage);
+            message.reply(embed);
+        } catch (error) {
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Bot Encountered an Error')
+            .setColor(0x25faf6)
+            message.reply(embed);
         }
-        
-        
-
-        const embed = new Discord.MessageEmbed()
-        .setTitle('Canceling Lab Hours:')
-        .setColor(0x25faf6)
-        .setDescription(returnMessage);
-        message.reply(embed);
-        return false;
     }
 
+    editHour(hour) {
+        if (hour >= 0 && hour <= 12) {
+            if ( hour == 0) {
+                hour = 12;
+            }
+            return hour + "AM";
+        } else {
+            hour -= 12;
+            return hour + "PM";
+        }
+    }
+
+    /**
+     * Un-Cancel Hours
+     * Sets hours to "Not Canceled"
+     * 
+     * @param {class} message Discord.js message object.
+     * @param {array} params Array of strings of other arguments from message.
+     */
+    async uncancel(message, params) {
+        try {
+            let returnMessage = "";
+            if (params.length == 3) {
+                let name = params[0].charAt(0).toUpperCase() + params[0].substring(1);
+                let ta = await TA.findOne({
+                    name: name,
+                });
+                let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+                const isSame = (element) => element == params[1];
+                let dayIndex = days.findIndex(isSame);
+                let event = null;
+                if (ta != null) {
+                    if (dayIndex != -1) {
+                        let hours = await LabHour.find({
+                            ta: ta._id,
+                        });
+                        for (let i = 0; i < hours.length; i++) {
+                            let start = new Date(hours[i].start);
+                            let day = start.getDay();
+                            let hour = start.getHours();
+                            if (day == dayIndex && hour == parseInt(params[2], 10)) {
+                                event = hours[i];
+                                await LabHour.updateOne({
+                                    _id: hours[i]._id,
+                                }, {
+                                    $set: {
+                                        canceled: false,
+                                    }
+                                });
+                                break;
+                            }
+                        }
+                        if (event) {
+                            let day = days[(new Date(event.start)).getDay()].charAt(0).toUpperCase() + days[(new Date(event.start)).getDay()].substring(1);
+                            let hour = new Date(event.start).getHours();
+                            returnMessage = "Success: Lab Hours at " + hour + " on " + day + " for **" + name + "** have been un-canceled";
+                        } else {
+                            returnMessage = "**Error**: No Lab Hour scheduled at that time.\n";
+                        }
+                    } else {
+                        returnMessage = "**Error**: Incorrect Day.\nTry: monday, tuesday, wednesday, thursday, friday, saturday";
+                    }
+                } else {
+                    returnMessage = "**Error**: TA does not exist.\nContact Andrew. He screwed up somewhere.";
+                }
+            } else {
+                returnMessage = "**Error**: Forgot a parameter.\n**Command**:`/cancel name day hour`\n**Example:** `/cancel bob tuesday 14`";
+            }
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Un-Canceling Lab Hours:')
+            .setColor(0x25faf6)
+            .setDescription(returnMessage);
+            message.reply(embed);
+        } catch (error) {
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Bot Encountered an Error')
+            .setColor(0x25faf6)
+            message.reply(embed);
+        }
+    }
+
+    /**
+     * Cancel Hours
+     * Sets hours to "Canceled"
+     * 
+     * @param {class} message Discord.js message object.
+     * @param {array} params Array of strings of other arguments from message.
+     */
+    async cancel(message, params) {
+        try {
+            let returnMessage = "";
+            if (params.length == 3) {
+                let name = params[0].charAt(0).toUpperCase() + params[0].substring(1);
+                let ta = await TA.findOne({
+                    name: name,
+                });
+                let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+                const isSame = (element) => element == params[1];
+                let dayIndex = days.findIndex(isSame);
+                let event = null;
+                if (ta != null) {
+                    if (dayIndex != -1) {
+                        let hours = await LabHour.find({
+                            ta: ta._id,
+                        });
+                        for (let i = 0; i < hours.length; i++) {
+                            let start = new Date(hours[i].start);
+                            let day = start.getDay();
+                            let hour = start.getHours();
+                            if (day == dayIndex && hour == parseInt(params[2], 10)) {
+                                event = hours[i];
+                                await LabHour.updateOne({
+                                    _id: hours[i]._id,
+                                }, {
+                                    $set: {
+                                        canceled: true,
+                                    }
+                                });
+                                break;
+                            }
+                        }
+                        if (event) {
+                            let day = days[(new Date(event.start)).getDay()].charAt(0).toUpperCase() + days[(new Date(event.start)).getDay()].substring(1);
+                            let hour = new Date(event.start).getHours();
+                            returnMessage = "Success: Lab Hours at " + hour + " on " + day + " for **" + name + "** has been canceled";
+                        } else {
+                            returnMessage = "**Error**: No Lab Hour scheduled at that time.\n";
+                        }
+                    } else {
+                        returnMessage = "**Error**: Incorrect Day.\nTry: monday, tuesday, wednesday, thursday, friday, saturday";
+                    }
+                } else {
+                    returnMessage = "**Error**: TA does not exist.\nContact Andrew. He screwed up somewhere.";
+                }
+            } else {
+                returnMessage = "**Error**: Forgot a parameter.\n**Cancel Command:**`/cancel name day hour`\n**Example:** /cancel bob tuesday 14";
+            }
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Canceling Lab Hours:')
+            .setColor(0x25faf6)
+            .setDescription(returnMessage);
+            message.reply(embed);
+            return false;
+        } catch (error) {
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Bot Encountered an Error')
+            .setColor(0x25faf6)
+            message.reply(embed);
+        }
+    }
+
+    /**
+     * Ta Schedule
+     * Shows if TA is on duty or next available.
+     * 
+     * @param {class} message Discord.js message object.
+     */
     async taSchedule(message) {
         try {
             let returnMessage = "";
-            let now = new Date();
             let hours = await LabHour.find();
             let current = [];
             for (let i = 0; i < hours.length; i++) {
@@ -176,6 +353,7 @@ class TaBot extends Discord.Client {
                 hours = await LabHour.find();
                 let next = [];
                 let distance = 0;
+                let now = new Date();
                 for (let i = 0; i < hours.length; i++) {
                     let diff = hours[i].start - now.getTime();
                     if (diff == distance && !hours[i].canceled) {
@@ -196,11 +374,14 @@ class TaBot extends Discord.Client {
             .setColor(0x25faf6)
             .setDescription(returnMessage);
             message.reply(embed);
-        } catch(error) {
-            console.log(error);
+        } catch (error) {
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Bot Encountered an Error')
+            .setColor(0x25faf6)
+            message.reply(embed);
         }
-        return false;
     }
-
 }
+
+// Export
 module.exports = TaBot;
